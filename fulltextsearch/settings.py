43937db65ccd5dafb332a55def3240ff47db5f38
mrlzla <mrlzlamgilev@gmail.com>
2016-06-10 00:58:36 +0300

    Add urls page and get index page

--- fulltextsearch/settings.py
+++ fulltextsearch/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'search',
-    'endless_pagination'
+    'endless_pagination',
+    'bootstrapform',
 )
 
 MIDDLEWARE_CLASSES = (
--- search/views.py
+++ search/views.py
@@ -6,7 +6,8 @@ import urllib
 from search_engine.htmlparser import Parser
 from search_engine.make_index import MakeIndex
 from search_engine.queries import Queries
-from endless_pagination.decorators import page_template
+from django.views.generic.edit import UpdateView
+import json
 
 def create_filename(url):
   return '_'.join(url.split('//')[1].split('/'))
@@ -26,7 +27,7 @@ def index(request, template = 'search/index.html', extra_context = None):
   else:
     form = SearchForm()
     results = []
-  context = {'form': form, 'results': results, 'page_template': 'search/result.html'}
+  context = {'form': form, 'results': results, 'page_template': 'search/results.html'}
   return render(request, template, context )
 
 def add_index(request, template = 'search/add_index.html', extra_context = None):
@@ -35,7 +36,7 @@ def add_index(request, template = 'search/add_index.html', extra_context = None)
     if form.is_valid():
       url = request.POST['url']
       title, html, normalized_html = create_html(url)
-      first_50_words = ' '.join(html.split()[:50])
+      first_50_words = ' '.join(html.split()[:20])
       s = SearchResult.objects.create(url=url, html=first_50_words, filename=create_filename(url),
        normalized_html=normalized_html, title=title)
       MakeIndex([s]).save_indexes()
@@ -43,9 +44,20 @@ def add_index(request, template = 'search/add_index.html', extra_context = None)
     form = SearchResultForm()
   return render(request, template, {'form': form})
 
-def urls(request, template = 'search/result.html'):
+def get_index(request,template = 'search/get_index.html'):
+  index = json.load(open('search_engine/indexes.json'))
+  return render(request, template, {'index': index})
+
+
+def urls(request, template = 'search/urls.html'):
   results = SearchResult.objects.order_by('-id')
   return render(request, template, {'results': results})
+
+class SearchResultUpdate(UpdateView):
+    model = SearchResult
+    success_url = '/search/urls'
+    template_name = 'search/form.html'
+    fields = ['title']
 
     
 
